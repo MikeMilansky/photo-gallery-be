79468ad4d17092fbf1701ad73ea9cbb40173bc28
Nikita Reshetnikov <mikemilansky@gmail.com>
2017-10-17 00:04:35 +0300

    Added dumb authorization

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/isAuthorized/:userId', function(req, res, next) {
+  console.log(req.params['userId']);
+  res.send({ isAuthorized: [62695238].indexOf(parseInt(req.params['userId'], 10)) >= 0}); // dirty tem solution
 });
 
 module.exports = router;
