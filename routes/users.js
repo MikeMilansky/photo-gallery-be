af09e2218e81a7ede0eb2c749f38266e6439f46b
Nikita Reshetnikov <mikemilansky@gmail.com>
2017-10-17 21:15:10 +0300

    Added new id

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/isAuthorized/:userId', function(req, res, next) {
   console.log(req.params['userId']);
-  res.send({ isAuthorized: [62695238].indexOf(parseInt(req.params['userId'], 10)) >= 0}); // dirty tem solution
+  res.send({ isAuthorized: [62695238, 38236142].indexOf(parseInt(req.params['userId'], 10)) >= 0}); // dirty tem solution
 });
 
 module.exports = router;
